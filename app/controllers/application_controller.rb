9362aed122820bd9ab3db293b7afaf247c1b2fd3
Timothy Ege <retibra@gmail.com>
2018-08-25 22:10:00 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,14 @@ class App < Sinatra::Base
     post '/teams' do
       @team_name = params[:team][:name]
       @team_motto = params[:team][:motto]
-      @names = []
-      @powers = []
-      @bios = []
+      @hero_name = []
+      @hero_power = []
+      @hero_bio = []
       @team_members = params[:team][:members]
-      @team_members.each do |tm|
-        @names << tm[:name]
-        @powers << tm[:power]
-        @bios << tm[:bio]
+      @team_members.each do |hero|
+        @hero_name << hero[:name]
+        @hero_power << hero[:power]
+        @hero_bio << hero[:bio]
       end
       erb :team
     end
